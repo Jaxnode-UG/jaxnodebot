0b5ac5e19badef6f4644bd10e2a293ef21cb26df
root <root@rapidseedbox.com>
2017-03-08 01:47:46 +0000

    Adding updates to stream

--- stream.js
+++ stream.js
@@ -14,11 +14,42 @@ const Twitter = new twit(config);
 var stream = Twitter.stream('statuses/filter', { track: ['jaxnode'] });
 
 stream.on('tweet', function (tweet) {
-  console.log(tweet.text);
   const tweetwords = tweet.text.split(' ');
-  const hasWhat = tweetwords.filter(w => w === 'when');
+  const hasWhat = tweetwords.filter(w => w.toUpperCase() === 'WHAT');
   if (hasWhat.length > 0) {
-      meeting.what(r => console.log(r));
+      meeting.what(r => {
+	const nextMeetingTopic = `@${tweet.user.screen_name} The next meeting is on '${r}'`;
+	tweetNow(nextMeetingTopic);
+      });
+  }
+  const hasWhen = tweetwords.filter(w => w.toUpperCase() === 'WHEN');
+  if (hasWhen.length > 0) {
+      meeting.when(w => {
+        const neetMeetingTime = `@${tweet.user.screen_name} Our next meeting will be on ${w}`;
+	tweetNow(neetMeetingTime);
+      });
+  }
+  const hasWhere = tweetwords.filter(w => w.toUpperCase() === 'WHERE');
+  if (hasWhere.length > 0) {
+      meeting.where(r => {
+	const meetingLocation = r;
+        tweetNow(`@${tweet.user.screen_name} ${r}`);
+      })
   }
   // meeting
 });
+
+function tweetNow(tweetTxt) {  
+    const tweet = {
+        status: tweetTxt
+    }
+    Twitter.post('statuses/update', tweet, function(err, data, response) {
+      if(err){
+        console.log('Error in Replying');
+        console.error(err);
+      }
+      else{
+        console.log('Gratitude shown successfully');
+      }
+    });
+}
